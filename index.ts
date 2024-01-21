1324e105452b4ed592af3e2b6d7941f2625c91a9
gigantino <giovanni.gigantino@atoms.retexspa.com>
2024-01-21 23:18:34 +0100

    feat: start experimenting with classic parser

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+/*
 import kyberna from "./parsers/kyberna";
 import type { KybernaResponse } from "./parsers/kyberna";
 
@@ -16,3 +17,40 @@ kybernaSites.forEach((url) => kybernaRequests.push(kyberna(url)));
 const kybernaResponse = await Promise.all(kybernaRequests);
 
 console.log(kybernaResponse.flat(1));
+*/
+
+import puppeteer from "puppeteer";
+
+interface AuctionItem {
+  numberOfOffers: number;
+  bidderName: string;
+}
+
+async function scrape() {
+  const browser = await puppeteer.launch({ headless: "new" });
+  const page = await browser.newPage();
+  await page.goto("https://ecari.vs.ch/ecari-auction/");
+
+  const selector = "#tabContent1 > div > div > div.carContent > table > tbody";
+
+  await page.waitForSelector(selector);
+
+  const elements = await page.$$(selector + " > tr");
+
+  const auctionItems: AuctionItem[] = [];
+
+  for (const elementHandle of elements) {
+    const tdValues = await page.evaluate((tr) => {
+      const tds = tr.querySelectorAll("td");
+      return tds ? Array.from(tds).map((td) => td.textContent.trim()) : null;
+    }, elementHandle);
+
+    tdValues?.forEach((v, i) => {
+      console.log(i, v);
+    });
+  }
+
+  await browser.close();
+}
+
+scrape();
